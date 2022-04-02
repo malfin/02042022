3c7edb44643992911539bb62e0b2608de9c58db8
Алексей <aiex.0115970@gmail.com>
2022-04-02 11:03:38 +0500

    For

---
+++ step_01.py
@@ -0,0 +1,22 @@
+# products = [1, 45, 87, 2, 5, 23, 9]
+#
+# seen_prods = [87, 1, 5]
+
+
+# target_prods = ?
+
+
+def filter_products(products, seen_prods):
+    target_prods = []
+    for item in products:
+        if item not in seen_prods:
+            target_prods.append(item)
+    return target_prods
+
+
+print(filter_products(
+    [1, 45, 87, 2, 5, 23, 9],
+    [87, 1, 5]
+))
+
+# [45, 2, 34, 9]
