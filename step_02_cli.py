cec1a87b59fadc8259fb5fb54357c317164e5105
Алексей <aiex.0115970@gmail.com>
2022-04-09 11:08:02 +0500

    Test #4

--- step_02_cli.py
+++ step_02_cli.py
@@ -5,7 +5,6 @@ import click
 @click.option('--number', prompt='Your number')
 def hello(numbers):
     subtraction = 0
-    """Simple program that greets NAME for a total of COUNT times."""
     numbers_list = list(map(int, numbers.split(',')))
     for x in numbers_list:
         subtraction -= x
