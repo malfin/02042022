13e6d10087f37c376d4e8a41a79dc77ec850ccd4
Алексей <aiex.0115970@gmail.com>
2022-04-09 11:45:34 +0500

    Test #9

--- step_02_cli_argparse.py
+++ step_02_cli_argparse.py
@@ -1,12 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-answer = 0
 parser.add_argument("square",
                     help="display a square of a given number", nargs='+')
-numbers_list = list(map(int, parser.parse_args().split(',')))
-print(numbers_list)
+subtraction = 0
 args = parser.parse_args()
-answer -= args.square
-
-print(answer)
+print(args.square)
+# answer -= args.square
+#
+# print(answer)
