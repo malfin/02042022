4ab13c242454dc3628bef040c0f1e0703bcc8a41
Алексей <aiex.0115970@gmail.com>
2022-04-09 11:57:19 +0500

    Test #11

--- step_02_cli_argparse.py
+++ step_02_cli_argparse.py
@@ -1,3 +1,10 @@
+"""
+USE CLI:
+    python step_02_cli_argparse.py '1,2,3,4,5'
+Linux and MacOS:
+    python3 step_02_cli_argparse.py '1,2,3,4,5'
+"""
+
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -7,9 +14,13 @@ args = parser.parse_args()
 # print(args.subtraction)
 
 for x in args.subtraction:
-    print(type(x))  # str
+    # print(type(x))  # str
     subtraction = 0
     expo = map(int, x.split(','))
     for i in expo:
         subtraction -= i
     print(subtraction)
+
+"""
+nargs - ввод нескольких чисел
+"""
